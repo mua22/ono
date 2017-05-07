1bfc89333a13d21dbb9c8e76f9b94d90a32dc02e
umairrafique23 <umair_rafiq@live.com>
2017-05-07 22:26:07 +0500

    Front Layout Updated
    Updated Front bar, navar and footer

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -2,36 +2,41 @@
 <html lang="en">
 <head>
     <!-- Latest compiled and minified CSS & JS -->
-    <link rel="stylesheet" media="screen" href="{{ URL::asset('css/site.css') }}">
+
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans:700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css" type="text/css">
+    <link rel="stylesheet" media="screen" href="{{ URL::asset('css/site.css') }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
     <meta charset="UTF-8">
     <title>Ono Site</title>
 </head>
-<style>.dropdown:hover .dropdown-menu {
-display: block;
-        padding: 0px;
-    }
-</style>
+
 <body>
 
-<div id="content" class="container">
+<header>
+    <div class="container">
 
-    <div class="row top-bar">
-        <button onclick="location.href = '/admin'" class="topAchor btn btn-primary pull-right">Admin</button>
+        <div id="branding">
+            <h1><span class="highlight">Ono</span> A dream job</h1>
+        </div>
+        <a href="/admin" class="btn btn-danger" style="float: right;"><i class="fa fa-lock" style="margin-right: 5px"></i>Admin</a>
+
     </div>
+</header>
+
+<div class="container">
+
+
     <div class="row">
-
-
-
-
-        <nav class="navbar navbar-inverse">
+        <nav class="navbar">
             <div class="container-fluid">
-                <div class="navbar-header">
-                    <p class="navbar-brand">ONO</p>
-                </div>
+                <div class="col-lg-6">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="/">Home</a></li>
 
@@ -53,8 +58,23 @@ display: block;
                         </li>
                     @endforeach
                 </ul>
+                </div><div class="col-lg-6" id="newsletter">
+                            <form class="form-inline">
+                                <div class="form-group">
+
+                                    <input class="form-control" type="text" placeholder="Search here...">
+
+
+                                    <button type="submit" class="button_1 form-control"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                </div>
+                            </form>
+
+                </div>
            </div>
+
+
         </nav>
+
 
 
     </div>
@@ -76,6 +96,9 @@ display: block;
     </div>
 
 </div>
+<footer>
+    <p>Ono, Copyright &copy; 2017</p>
+</footer>
 
 </body>
 </html>
